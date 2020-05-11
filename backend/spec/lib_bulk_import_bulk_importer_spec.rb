b119dbd03e0b65d0662a5be0e9cc51a45b8fafdb
Bobbi Fox <bobbi@bobbifox.net>
2020-05-11 17:45:32 -0400

    remove repo creation

--- backend/spec/lib_bulk_import_bulk_importer_spec.rb
+++ backend/spec/lib_bulk_import_bulk_importer_spec.rb
@@ -5,7 +5,6 @@ require_relative "../app/controllers/lib/bulk_import/bulk_importer.rb"
 describe "Bulk Importer" do
   BULK_FIXTURES_DIR = File.join(File.dirname(__FILE__), "fixtures", "bulk_import")
   before(:each) do
-    create(:repo)
     @current_user = User.find(:username => "admin")
     # create the resource
     resource = JSONModel(:resource).from_hash("title" => "a resource",
